4e936b85763edd1c6861cdd087a3c32fa5e1f0c6
Jiantao <144024751+jiantaoshen@users.noreply.github.com>
2026-03-16 15:10:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿decimal totalMoney = 0.00m;
+
+Console.WriteLine("Welcome to TrackMoney");
+Console.WriteLine($"You have currently {totalMoney} Kr on your account.");
+Console.WriteLine("Pick an option:");
+
+Console.WriteLine("(1) Show Items (All/Expense(s)/income(s))");
+Console.WriteLine("(2) Add New Expense/Income)");
+Console.WriteLine("(3) Edit Item (Edit, Remove)");
+Console.WriteLine("(4) Save and Quit");
